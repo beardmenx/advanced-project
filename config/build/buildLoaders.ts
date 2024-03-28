5e7437b3e3b5af579290c2c86cc66a336b2211d9
Vitalii <beaedmenx@gmail.com>
2024-03-28 12:32:40 +0100

    config and add MiniCssExtractPlugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,14 +1,26 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BuildOptions } from 'typescript';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
-      // Creates `style` nodes from JS strings
-      'style-loader',
-      // Translates CSS into CommonJS
-      'css-loader',
-      // Compiles Sass to CSS
+      // Очень важен порядок лоудеров !!!!!!!!
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resourcePath: string) =>
+              Boolean(resourcePath.includes('.module.')),
+            localIdentName: isDev
+              ? '[path][name]__[local]--[hash:base64:5]'
+              : '[hash:base64:8]',
+          },
+        },
+      },
+
       'sass-loader',
     ],
   };
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import './Counter.scss';
+import s from './Counter.module.scss';
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
   return (
-    <div>
+    <div className={s.btn}>
       <h1>{count}</h1>
       <button onClick={increment}>increment</button>
     </div>
